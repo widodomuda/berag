a19a38a201bf1f4eb662770416d974096a4b7793
widodomuda <38934299+widodomuda@users.noreply.github.com>
2026-01-20 09:52:01 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ apt install curl
 sudo apt-get install zstd
 curl -fsSL https://ollama.com/install.sh | sh
 ollama serve &
+ollama pull llama3.2:3b
 pip3 install "numpy<2.0"
 pip3 install --use-deprecated=legacy-resolver sentence-transformers
 pip3 install "huggingface-hub>=0.34.0,<1.0"
